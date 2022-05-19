773d2b8c9dc387cea172bcd2356fb9a1ba05d935
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-05-19 15:44:12 -0700

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 //creating game loop first 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
+
+let head1 =  1
+let head2 = 1
+const snakebody = []
+
+
 functiondrawGame(){
+
+
+
+
+
 
 setTimeout(drawGame,100/ speed)
 //this will adjust movement speed later
