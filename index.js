b4e6318c807ffabff77c61fe6ed76769b6e46041
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-05-19 17:01:43 -0700

    new

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
 let speed = 5
-let tileCount = 100
-let tileSize = canvas.width / tileCount;
+let tileCount = 50
+let tileSize = canvas.width / tileCount  -5;
 let snake = [headx,heady,snakebody,tail]
-let headx =  1
-let heady = 1
+let headx =  10
+let heady = 10
 const snakebody = []
 let tail = 0
 
@@ -30,7 +30,7 @@ function clearScreen() {
 }
 
 function drawSnake(){
-    ctx.fillStyle = 'light-blue'
+    ctx.fillStyle = 'blue'
 
     for(let i=0; i < snakebody.length; i++) {
         let part = snakebody[i]
@@ -38,6 +38,18 @@ function drawSnake(){
 }
 
 
+
+document.body.addEventListener('keydown', keyDown);
+FUNCTION keyDown(event){
+
+
+}
+            if (event.keyDown)
+
+
+
+
+// keycodes (W,A,S,D) (87,65,83,68)/////////(UP,RIGHT,DOWN,LEFT)   (38,39,40,37)
 clearScreen();
 drawGame();
 setTimeout();
