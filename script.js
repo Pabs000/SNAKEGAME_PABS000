157835d50f2ae15692c5473183a2354f5a31055b
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2023-07-01 10:14:58 -0700

    new

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+{
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-class SnakePart {}{
-  constructor(x,y) 
-  this.x =x;
-  this.y = y;
-  }
-  
-let SnakeHeadX =  20;
-let SnakeHeadY = 20;
-const SnakeBody = [];
-let Tail = 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
